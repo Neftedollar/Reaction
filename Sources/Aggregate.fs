a03a500a6d3f05b7aed06858c5bb0e649ea6ade5
Dag Brattli <dag@brattli.net>
2018-08-31 08:26:43 +0200

    Add initial impl. of groupBy operator

--- Sources/Aggregate.fs
+++ Sources/Aggregate.fs
@@ -25,3 +25,52 @@ module Aggregate =
 
     let scan (initial : 's) (accumulator: 's -> 'a -> 's) (source : AsyncObservable<'a>) : AsyncObservable<'s> =
         scanAsync initial (fun s x -> async { return accumulator s x } ) source
+
+    let groupBy (groupSelector: 'a -> 'g) (source : AsyncObservable<'a>) : AsyncObservable<AsyncObservable<'a>> =
+        let subscribe (aobv : AsyncObserver<AsyncObservable<'a>>) =
+            let agent = MailboxProcessor.Start(fun inbox ->
+                let rec messageLoop (groups: Map<'g, AsyncObserver<'a>>) = async {
+                    let! n = inbox.Receive ()
+
+                    let! newGroups =
+                        async {
+                            match n with
+                            | OnNext x ->
+                                let groupKey = groupSelector x
+                                let! newGroups = async {
+                                    match groups.TryFind groupKey with
+                                    | Some group ->
+                                        do! group n
+                                        return groups
+                                    | None ->
+                                        let obv, obs = Streams.stream ()
+                                        return groups.Add (groupKey, obv)
+                                }
+                                return newGroups
+                            | OnError ex ->
+                                for entry in groups do
+                                    do! OnError ex |> entry.Value
+                                do! OnError ex |> aobv
+                                return Map.empty
+                            | OnCompleted ->
+                                for entry in groups do
+                                    do! OnCompleted |> entry.Value
+                                do! aobv OnCompleted
+                                return Map.empty
+                        }
+
+                    return! messageLoop newGroups
+                }
+
+                messageLoop Map.empty
+            )
+
+            async {
+                let obv (n : Notification<'a>) =
+                    async {
+                        agent.Post n
+                    }
+
+                return! source obv
+            }
+        subscribe
